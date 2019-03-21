f713d8195beb5666861982ed64dc086d9e0534ad
朱强龙 <gzzhuqianglong@corp.netease.com>
2019-03-21 19:39:38 +0800

    提交HostApp层的代码

---
+++ app/src/main/java/com/example/hostapp/ContainerActivity.kt
@@ -0,0 +1,89 @@
+package com.example.hostapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.FrameLayout
+import io.flutter.facade.Flutter
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.view.FlutterView
+
+/**
+ * Created by Yagami3zZ hiqlong@163.com on 2019/3/21 0021.
+ * Description:
+ */
+class ContainerActivity : AppCompatActivity() {
+
+    companion object {
+        const val CHANNEL_NAME = "com.example.native.data"
+        const val ROUTE_DATA = "route_data"
+        const val NATIVE_DATA = "[\n" +
+                "  {\n" +
+                "    \"userId\": 1,\n" +
+                "    \"id\": 1,\n" +
+                "    \"title\": \"sunt aut facere repellat provident occaecati excepturi optio reprehenderit\",\n" +
+                "    \"body\": \"quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"userId\": 1,\n" +
+                "    \"id\": 2,\n" +
+                "    \"title\": \"qui est esse\",\n" +
+                "    \"body\": \"est rerum tempore vitae\\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\\nqui aperiam non debitis possimus qui neque nisi nulla\"\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"userId\": 1,\n" +
+                "    \"id\": 3,\n" +
+                "    \"title\": \"ea molestias quasi exercitationem repellat qui ipsa sit aut\",\n" +
+                "    \"body\": \"et iusto sed quo iure\\nvoluptatem occaecati omnis eligendi aut ad\\nvoluptatem doloribus vel accusantium quis pariatur\\nmolestiae porro eius odio et labore et velit aut\"\n" +
+                "  }]"
+    }
+
+    private lateinit var routeData: String
+
+    private lateinit var mFlutterLayout: FrameLayout
+
+    private lateinit var flutterView: FlutterView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_container)
+
+        routeData = intent.getStringExtra(ROUTE_DATA)
+
+        mFlutterLayout = findViewById(R.id.container)
+
+        mFlutterLayout.removeAllViews()
+
+        flutterView = Flutter.createView(
+            this@ContainerActivity,
+            lifecycle,
+            routeData
+        )
+
+        val layoutParams = FrameLayout.LayoutParams(
+            FrameLayout.LayoutParams.MATCH_PARENT,
+            FrameLayout.LayoutParams.MATCH_PARENT
+        )
+        mFlutterLayout.addView(flutterView, layoutParams)
+
+        val listeners = arrayOfNulls<FlutterView.FirstFrameListener>(1)
+        listeners[0] = FlutterView.FirstFrameListener {
+            mFlutterLayout.visibility = View.VISIBLE
+        }
+        flutterView.addFirstFrameListener(listeners[0])
+
+        MethodChannel(flutterView, CHANNEL_NAME).setMethodCallHandler(object : MethodChannel.MethodCallHandler {
+            override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+                if (call.method!!.contentEquals("getNativeData")) {
+                    result.success(getNativeData())
+                }
+            }
+        })
+    }
+
+    fun getNativeData() :String{
+        return NATIVE_DATA
+    }
+
+}
--- app/src/main/java/com/example/hostapp/MainActivity.kt
+++ app/src/main/java/com/example/hostapp/MainActivity.kt
@@ -1,51 +1,110 @@
 package com.example.hostapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.FrameLayout
+import android.widget.LinearLayout
 import io.flutter.facade.Flutter
-
-
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.view.FlutterView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var flutterView: FlutterView
+
+    var hostData = "这是一条来自Host的数据"
+
+    private lateinit var mOriginLayout: LinearLayout
+    private lateinit var mFlutterLayout: FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Example of a call to a native method
-//        button1.text = stringFromJNI()
+        mOriginLayout = findViewById(R.id.origin_layout)
+        mFlutterLayout = findViewById(R.id.container)
+
+        val listeners = arrayOfNulls<FlutterView.FirstFrameListener>(1)
+        listeners[0] = FlutterView.FirstFrameListener {
+            mFlutterLayout.visibility = View.VISIBLE
+            mOriginLayout.visibility = View.GONE
+        }
+
+        button0.setOnClickListener {
+            jumpTo("route1")
+        }
+
         button1.setOnClickListener {
-            val flutterView = Flutter.createView(
+            jumpTo("route2")
+        }
+
+        button2.setOnClickListener {
+            mFlutterLayout.removeAllViews()
+
+            flutterView = Flutter.createView(
                 this@MainActivity,
                 lifecycle,
                 "route1"
             )
-            val layout = FrameLayout.LayoutParams(600, 800)
-            layout.leftMargin = 100
-            layout.topMargin = 200
-            addContentView(flutterView, layout)
+
+            val layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+            )
+            mFlutterLayout.addView(flutterView, layoutParams)
+
+            flutterView.addFirstFrameListener(listeners[0])
+
+            MethodChannel(flutterView, "app.channel.shared.data").setMethodCallHandler(object : MethodChannel.MethodCallHandler {
+                override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+                    if (call.method!!.contentEquals("getSharedText")) {
+                        result.success(hostData)
+                    }
+                }
+            })
         }
 
-        button2.setOnClickListener {
-            val tx = supportFragmentManager.beginTransaction()
-            tx.replace(R.id.container, Flutter.createFragment("route1"))
-            tx.commit()
+        button3.setOnClickListener {
+//            val tx = supportFragmentManager.beginTransaction()
+//            tx.replace(R.id.container, Flutter.createFragment("route2"))
+//            tx.commit()
+
+            mFlutterLayout.removeAllViews()
+
+            flutterView = Flutter.createView(
+                this@MainActivity,
+                lifecycle,
+                "route2"
+            )
+
+            val layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT
+            )
+            mFlutterLayout.addView(flutterView, layoutParams)
+
+            flutterView.addFirstFrameListener(listeners[0])
+
+            MethodChannel(flutterView, "app.channel.shared.data").setMethodCallHandler(object :
+                MethodChannel.MethodCallHandler {
+                override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+                    if (call.method!!.contentEquals("getSharedText")) {
+                        result.success(hostData)
+                    }
+                }
+            })
         }
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
-
-    companion object {
-
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
-        }
+    private fun jumpTo(routeData: String) {
+        val intent = Intent()
+        intent.putExtra(ContainerActivity.ROUTE_DATA, routeData)
+        intent.setClass(this@MainActivity, ContainerActivity::class.java)
+        startActivity(intent)
     }
+
 }
