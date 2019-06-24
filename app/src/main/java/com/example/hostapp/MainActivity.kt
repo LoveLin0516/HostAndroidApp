a45445f46109247a0fb1b789153d97a26db246f3
yagamii <gzzhuqianglong@corp.netease.com>
2019-06-24 18:29:22 +0800

    修改打包配置和代码逻辑，使用Flutterlab中的代码进行

--- app/src/main/java/com/example/hostapp/MainActivity.kt
+++ app/src/main/java/com/example/hostapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.FrameLayout
 import android.widget.LinearLayout
+import android.widget.Toast
 import io.flutter.facade.Flutter
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -20,6 +21,35 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mOriginLayout: LinearLayout
     private lateinit var mFlutterLayout: FrameLayout
+
+    companion object {
+
+        const val Channel_Sweeper_About = "Channel_Sweeper_About"
+        const val Channel_Global_Bridge = "Channel_Global_Bridge"
+        const val Channel_Sweeper_Offline = "Channel_Sweeper_Offline"
+
+        const val Route_About_Sweeper = "Route_About_Sweeper"
+        const val Route_Sweeper_Offline = "Route_Sweeper_Offline"
+
+        const val Method_Get_Sweeper_Name = "getSweeperName"
+        const val Method_Get_Serial_Num = "getSerialNum"
+        const val Method_Get_Device_Version = "getDeviceVersion"
+        const val Method_Get_Which_Native = "getWhichNative"
+        const val Method_Get_Offline_Title = "getOfflineTitle"
+        const val Method_Close_Current_Page = "closeCurrentPage"
+
+        const val Method_Config_NetWork = "configNetWork"
+        const val Method_Reset_Device = "resetDevice"
+        const val Method_Copy_Serial_Num = "copySerialNum"
+
+        const val Route_Data = "route"
+
+        const val Sweeper_Name = "Sweeper_Name"
+        const val Serial_Num = "Serial_Num"
+        const val Device_Version = "Device_Version"
+        const val Offline_Title = "Offline_Title"
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,17 +89,40 @@ class MainActivity : AppCompatActivity() {
 
             flutterView.addFirstFrameListener(listeners[0])
 
-            MethodChannel(flutterView, "app.channel.shared.data").setMethodCallHandler(object : MethodChannel.MethodCallHandler {
+            MethodChannel(flutterView, Channel_Sweeper_About).setMethodCallHandler(object :
+                MethodChannel.MethodCallHandler {
                 override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-                    if (call.method!!.contentEquals("getSharedText")) {
-                        result.success(hostData)
+                    when {
+                        call.method!!.contentEquals("getSharedText") -> result.success(hostData)
+                        call.method!!.contentEquals(Method_Get_Sweeper_Name) -> result.success(getSweeperName())
+                        call.method!!.contentEquals(Method_Get_Serial_Num) -> result.success(getSerialNum())
+                        call.method!!.contentEquals(Method_Get_Device_Version) -> result.success(getDeviceVersion())
+
+                        call.method!!.contentEquals(Method_Config_NetWork) -> {
+                            configNetWork()
+                        }
+                        call.method!!.contentEquals(Method_Reset_Device) -> {
+                            resetDevice()
+                        }
+                        call.method!!.contentEquals(Method_Copy_Serial_Num) -> {
+                            copySerialNum()
+                        }
+                    }
+                }
+            })
+
+            MethodChannel(flutterView, Channel_Global_Bridge).setMethodCallHandler(object :
+                MethodChannel.MethodCallHandler {
+                override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+                    when {
+                        call.method!!.contentEquals(Method_Get_Which_Native) -> result.success("Android")
                     }
                 }
             })
         }
 
         button3.setOnClickListener {
-//            val tx = supportFragmentManager.beginTransaction()
+            //            val tx = supportFragmentManager.beginTransaction()
 //            tx.replace(R.id.container, Flutter.createFragment("route2"))
 //            tx.commit()
 
@@ -106,6 +159,50 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra(TestContainerActivity2.ROUTE_DATA, routeData)
         intent.setClass(this@MainActivity, TestContainerActivity2::class.java)
         startActivity(intent)
+    }
+
+    /**
+     * 获取扫地机名称
+     */
+    fun getSweeperName(): String {
+        return "智能激光扫地机"
+    }
+
+    /**
+     * 获取序列号
+     */
+    fun getSerialNum(): String {
+        return "12234566"
+    }
+
+    /**
+     * 获取硬件版本号
+     */
+    fun getDeviceVersion(): String {
+        return "2.2.39"
+    }
+
+    /**
+     * 配置网络
+     */
+    //功能调整放到外面页面了
+    fun configNetWork() {
+
+    }
+
+    /**
+     * 恢复出厂设置
+     */
+    //功能调整放到外面页面了
+    fun resetDevice() {
+
+    }
+
+    /**
+     * 复制设备序列号
+     */
+    fun copySerialNum() {
+        Toast.makeText(this, "序列号复制成功", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/example/hostapp/TestContainerActivity2.kt
+++ app/src/main/java/com/example/hostapp/TestContainerActivity2.kt
@@ -3,6 +3,7 @@ package com.example.hostapp
 import android.os.Bundle
 import android.view.View
 import android.widget.FrameLayout
+import android.widget.Toast
 import io.flutter.app.FlutterActivity
 import io.flutter.app.FlutterFragmentActivity
 import io.flutter.facade.Flutter
@@ -19,6 +20,32 @@ import io.flutter.view.FlutterView
 class TestContainerActivity2 : FlutterActivity() {
 
     companion object {
+
+        const val Channel_Sweeper_About = "Channel_Sweeper_About"
+        const val Channel_Global_Bridge = "Channel_Global_Bridge"
+        const val Channel_Sweeper_Offline = "Channel_Sweeper_Offline"
+
+        const val Route_About_Sweeper = "Route_About_Sweeper"
+        const val Route_Sweeper_Offline = "Route_Sweeper_Offline"
+
+        const val Method_Get_Sweeper_Name = "getSweeperName"
+        const val Method_Get_Serial_Num = "getSerialNum"
+        const val Method_Get_Device_Version = "getDeviceVersion"
+        const val Method_Get_Which_Native = "getWhichNative"
+        const val Method_Get_Offline_Title = "getOfflineTitle"
+        const val Method_Close_Current_Page = "closeCurrentPage"
+
+        const val Method_Config_NetWork = "configNetWork"
+        const val Method_Reset_Device = "resetDevice"
+        const val Method_Copy_Serial_Num = "copySerialNum"
+
+        const val Route_Data = "route"
+
+        const val Sweeper_Name = "Sweeper_Name"
+        const val Serial_Num = "Serial_Num"
+        const val Device_Version = "Device_Version"
+        const val Offline_Title = "Offline_Title"
+
         const val CHANNEL_NAME = "com.example.native.data"
         const val ROUTE_DATA = "route"
         const val NATIVE_DATA = "[\n" +
@@ -42,6 +69,7 @@ class TestContainerActivity2 : FlutterActivity() {
                 "  }]"
     }
 
+
     private lateinit var routeData: String
 
     private lateinit var mFlutterLayout: FrameLayout
@@ -63,10 +91,31 @@ class TestContainerActivity2 : FlutterActivity() {
 //
 //        flutterView.addFirstFrameListener(listeners[0])
 
-        MethodChannel(flutterView, CHANNEL_NAME).setMethodCallHandler(object : MethodChannel.MethodCallHandler {
+        MethodChannel(flutterView, Channel_Sweeper_About).setMethodCallHandler(object : MethodChannel.MethodCallHandler {
             override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-                if (call.method!!.contentEquals("getNativeData")) {
-                    result.success(getNativeData())
+                when {
+                    call.method!!.contentEquals("getNativeData") -> result.success(getNativeData())
+                    call.method!!.contentEquals(MainActivity.Method_Get_Sweeper_Name) -> result.success(getSweeperName())
+                    call.method!!.contentEquals(MainActivity.Method_Get_Serial_Num) -> result.success(getSerialNum())
+                    call.method!!.contentEquals(MainActivity.Method_Get_Device_Version) -> result.success(getDeviceVersion())
+
+                    call.method!!.contentEquals(MainActivity.Method_Config_NetWork) -> {
+                        configNetWork()
+                    }
+                    call.method!!.contentEquals(MainActivity.Method_Reset_Device) -> {
+                        resetDevice()
+                    }
+                    call.method!!.contentEquals(MainActivity.Method_Copy_Serial_Num) -> {
+                        copySerialNum()
+                    }
+                }
+            }
+        })
+
+        MethodChannel(flutterView, Channel_Global_Bridge).setMethodCallHandler(object : MethodChannel.MethodCallHandler {
+            override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+                when {
+                    call.method!!.contentEquals(Method_Get_Which_Native) -> result.success("Android")
                 }
             }
         })
@@ -74,6 +123,50 @@ class TestContainerActivity2 : FlutterActivity() {
 
     fun getNativeData() :String{
         return NATIVE_DATA
+    }
+
+    /**
+     * 获取扫地机名称
+     */
+    fun getSweeperName(): String {
+        return "智能激光扫地机"
+    }
+
+    /**
+     * 获取序列号
+     */
+    fun getSerialNum(): String {
+        return "12234566"
+    }
+
+    /**
+     * 获取硬件版本号
+     */
+    fun getDeviceVersion(): String {
+        return "2.2.39"
+    }
+
+    /**
+     * 配置网络
+     */
+    //功能调整放到外面页面了
+    fun configNetWork() {
+
+    }
+
+    /**
+     * 恢复出厂设置
+     */
+    //功能调整放到外面页面了
+    fun resetDevice() {
+
+    }
+
+    /**
+     * 复制设备序列号
+     */
+    fun copySerialNum() {
+        Toast.makeText(this, "序列号复制成功", Toast.LENGTH_SHORT).show()
     }
 
 }
